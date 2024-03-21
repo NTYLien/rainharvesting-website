d0b938b422b900a777141e1f275a693877b5e9cb
TTCTien <camtientran0108@gmail.com>
2024-03-21 10:49:28 +0700

    update

--- payment.js
+++ payment.js
@@ -1,6 +1,21 @@
+function validateForm() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var address = document.getElementById('address').value;
+    // ... thêm các trường cần kiểm tra
+  
+    if (name && email && address) {
+      // Nếu tất cả các trường đã được nhập
+      window.location.href = 'confirmation_page.html'; // Chuyển hướng đến trang xác nhận
+    } else {
+      alert('Please enter complete information before payment!');
+    }
+  }
+  
+  
 document.getElementById('paymentForm').addEventListener('submit', function(e) {
     e.preventDefault();
     // Xác thực các trường thông tin
     // Xử lý thanh toán
-    alert('Đã tiến hành thanh toán!');
+    alert('payment has been made!');
 });
